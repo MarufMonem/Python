33ae300949b7e8b39505acd3045a7f044501de05
Maruf_Monem <marufmonem@gmail.com>
2023-10-22 12:40:00 +0600

    String and related arrays

---
+++ Day4.py
@@ -0,0 +1,15 @@
+# Variables
+
+# In python we dont need to mention the variable type
+
+x=2
+y=3
+print("X+Y : ", x+y)
+
+# Array
+content = "Youtube"
+print(content[2])
+
+print(content[-5])
+
+print(content[3:6])
