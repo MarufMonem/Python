f1cccf374ff01165f5909973dc470272e774d7ad
Maruf_Monem <marufmonem@gmail.com>
2023-10-22 11:48:02 +0600

    Day 3: Different operators

---
+++ Day3.py
@@ -0,0 +1,13 @@
+# Operators
+print("Addition with divison and float", 3 +2/4)
+print("Power: ", 3**3)
+print("String: ", 'With a single quote')
+print("String: ", "With a double quote's")
+print("String: ", 'Escape character: quote\"s')
+print("String: ", 'Escape character: quote\'s')
+print(10 * "Maruf ")
+print(10 * "new line \n")
+
+
+# skip the python special character
+print(r'\name')
