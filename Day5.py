1343914e51718aa790e8f61ca5a32e6eb3cd9d72
Maruf_Monem <marufmonem@gmail.com>
2023-10-23 09:13:29 +0600

    Day 5: Lists

---
+++ Day5.py
@@ -0,0 +1,21 @@
+#Lists
+nums = [10,20,30,40, "ABC"]
+
+print(nums)
+
+names = ["ACD", "Anik", "Maruf", "Monem"]
+
+combined = [nums, names]
+print(combined)
+
+nums.append("50")
+nums.insert(2, 77)
+
+print(nums)
+
+nums.pop()
+nums.pop()
+
+print(nums)
+print(min(nums))
+print(min(names)) -> counts the characters
