dab50f9ff92371d6279ad7b13426694e650bcda7
Maruf_Monem <marufmonem@gmail.com>
2023-10-23 10:47:47 +0600

    Day 10: Data types

---
+++ Day10.py
@@ -0,0 +1,16 @@
+#Data Types
+
+#Complex
+
+val = 6+2j # the "i" in complex values is replaced with j
+print(type(val))
+
+#coversion
+floatVal = 2.3
+print(int(floatVal))
+
+# Range
+range(0, 10)
+print(list(range(0,10)))
+print(list(range(0,10, 2)))
+print(list(range(10,0, -5)))
