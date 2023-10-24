da7ae1b501d50361266c6fe1727224b68b9c224f
Maruf_Monem <marufmonem@gmail.com>
2023-10-24 11:50:33 +0600

    Day 21: For loops

---
+++ Day21.py
@@ -0,0 +1,12 @@
+# for loops
+
+x=[1,2,6,6,32,78,23,44576,5,78658]
+
+for i in x:
+    #i is each element in X
+    print(i, end=" ")
+print()
+
+for i in range(5,15):
+    if i%5==0:
+        print(i , end=" " )
