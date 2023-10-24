3ccf6626b51ce2695c1e97e5dc0a125e488f6f30
Maruf_Monem <marufmonem@gmail.com>
2023-10-24 09:46:45 +0600

    Day 11: Operators

---
+++ Day11.py
@@ -0,0 +1,29 @@
+# Operators
+x=2
+x*=2
+print(x)
+
+x+=2
+print(x)
+
+x -=2
+print(x)
+
+x,y = 1,2
+print(x, y)
+
+x=-x
+print(x)
+
+# and operator
+result = x < 0 and y>0
+print(result)
+
+# OR
+result = x < 0 or y>10
+print(result)
+
+# Negate
+print(not result)
+
+
