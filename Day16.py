aed06be99e40f4ee0bcee732deb60b9d9391a1ea
Maruf_Monem <marufmonem@gmail.com>
2023-10-24 10:43:45 +0600

    Math operators

---
+++ Day16.py
@@ -0,0 +1,16 @@
+#math operators
+import math
+x=9
+y=30
+z=3
+x=math.sqrt(x)
+y=math.sqrt(y)
+print(x)
+print(y)
+print(math.floor(y))
+print(math.ceil(y))
+print(math.pow(z,2))
+print(z**2)
+
+# specific functions from math
+from math import sqrt, pow
