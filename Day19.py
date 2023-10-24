587a7cca876481b5d58bf7eec2eb1ca145508a18
Maruf_Monem <marufmonem@gmail.com>
2023-10-24 11:27:48 +0600

    Day19: Conditional statements: if else elfi

---
+++ Day19.py
@@ -0,0 +1,9 @@
+val = int(input("Please provide a value\n"))
+
+if val> 10:
+    print("Larger than 10")
+elif val>0:
+    print("Positive value")
+else:
+    print("Negative value")
+print("Execution completed")
